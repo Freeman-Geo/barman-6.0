49a304f22dfc24c0a83cecf14e43048d26f820f0
Geo Freeman <102118107+Freeman-Geo@users.noreply.github.com>
2024-06-15 15:34:20 -0600

    1er commit
    Faltan demasiadas imágenes e información de licores

---
+++ scripts.js
@@ -0,0 +1,352 @@
+const shaker = document.getElementById("prep-img-shaker")
+const stire = document.getElementById("prep-img-stire")
+const blender = document.getElementById("prep-img-blander")
+
+const iceBall = document.getElementById("ice-img-ball")
+const smallIce = document.getElementById("ice-img-reg-ice")
+const NormalSizeIce = document.getElementById("ice-img-small-ice")
+
+const water = document.getElementById("img-soda-water")
+const cocaCola = document.getElementById("img-soda-cocaCola")
+const delaware = document.getElementById("img-soda-delaware")
+const fanta = document.getElementById("img-soda-fanta")
+const gingerAle = document.getElementById("img-soda-gingerAle")
+const mirinda = document.getElementById("img-soda-mirinda")
+const pepsiCola = document.getElementById("img-soda-pepsiCola")
+const rootBeer = document.getElementById("img-soda-rootBeer")
+const sevenUp = document.getElementById("img-soda-sevenUp")
+const sprite = document.getElementById("img-soda-sprite")
+const squirt = document.getElementById("img-soda-squirt")
+const tehuacan = document.getElementById("img-soda-tehuacan")
+const tonica = document.getElementById("img-soda-tonica")
+
+const blueberryJuice = document.getElementById("blueberry-juice")
+const calahua = document.getElementById("calahua")
+const cinnamonTee = document.getElementById("cinnamon-tee")
+const cranberryJuice = document.getElementById("cranberry-juice")
+const grapeJuice = document.getElementById("grape-juice")
+const lemonJuice = document.getElementById("lemon-juice")
+const limeJuice = document.getElementById("lime-juice")
+const mangoeJuice = document.getElementById("mangoe-juice")
+const orangeJuice = document.getElementById("orange-juice")
+const pomeloJuice = document.getElementById("pomelo-juice")
+const tomatoeJuice = document.getElementById("tomatoe-juice")
+
+const simpleSyrup = document.getElementById("img-syrup-simple-syrup")
+const granadine = document.getElementById("img-syrup-granadine")
+
+const angostura = document.getElementById("img-add-angostura")
+const azucar = document.getElementById("img-add-azucar")
+const cafe = document.getElementById("img-add-cafe")
+const hierbabuena = document.getElementById("img-add-hierbabuena")
+const salsaInglesa = document.getElementById("img-add-inglesa")
+const jugoMaggi = document.getElementById("img-add-maggi")
+const menta = document.getElementById("img-add-menta")
+const milk = document.getElementById("img-add-milk")
+const olives = document.getElementById("img-add-olives")
+const sal = document.getElementById("img-add-sal")
+const tabasco = document.getElementById("img-add-tabasco")
+const vanilla = document.getElementById("img-add-vanilla")
+
+const calizGlass = document.getElementById("ing-glass-caliz-glass")
+const daiquiriGlass = document.getElementById("ing-glass-daiquiri-glass")
+const highGlass = document.getElementById("ing-glass-high-glass")
+const margaritaGlass = document.getElementById("ing-glass-margarita-glass")
+const martiniGlass = document.getElementById("ing-glass-martini-glass")
+const rockGlass = document.getElementById("ing-glass-rock-glass")
+const tequilaGlass = document.getElementById("ing-glass-tequila-glass")
+const tropicalGlass = document.getElementById("ing-glass-tropical-glass")
+
+const conEscarcha = document.getElementById("img-frost-con-escarcha")
+const sinEscarcha = document.getElementById("img-frost-sin-escarcha")
+
+const adornoApio = document.getElementById("img-dec-apio-adorno")
+const adornoCherry = document.getElementById("img-dec-cherry-adorno")
+const adornoHierbabuena = document.getElementById("img-dec-hierbabuena-adorno")
+const adornoLemonTwist = document.getElementById("img-dec-lemon-twist-adorno")
+const adornoLimeSlice = document.getElementById("img-dec-lime-slice-adorno")
+const adornoMenta = document.getElementById("img-dec-menta-adorno")
+const adornoOrangeSlice = document.getElementById("img-dec-orange-slice-adorno")
+const adornoOrangeTwist = document.getElementById("img-dec-orange-twist-adorno")
+const adornoPinapleSlice = document.getElementById("img-dec-pinaple-slice-adorno")
+const adornoCoffeeGrains = document.getElementById("img-dec-coffee-grains-adorno")
+
+const rumBacardiBco = document.getElementById("bottle-img-rum-bacardi-bco")
+const darkRumCaribbeanPearl = document.getElementById("bottle-img-dark-rum-caribbean-pearl")
+const tequilaBcoDonJulioBco = document.getElementById("bottle-img-tequila-bco-Don-Julio-70")
+const tequilaRepDonJulio = document.getElementById("bottle-img-tequila-rep-Don-Julio")
+const ginBeefeater = document.getElementById("bottle-img-gin-beefeater")
+const vodkaSky = document.getElementById("bottle-img-vodka-sky")
+const tripSecControy = document.getElementById("bottle-img-trip-sec-controy")
+const cubanRumHavanaClub = document.getElementById("bottle-img-cuban-spicy-rum-havanaClub")
+const brandiTorres10 = document.getElementById("bottle-img-brandi-Torres10")
+const campari = document.getElementById("bottle-img-campari")
+const amaretoDisaronno = document.getElementById("bottle-img-amareto-disaronno")
+const curacaoFlamingo = document.getElementById("bottle-img-curacao-flamingo")
+const licorAnisDulceMico = document.getElementById("licor-anis-dulce-mico")
+const licorCafeKahlua = document.getElementById("bottle-img-kahlua")
+const licorCocoMalibu = document.getElementById("bottle-img-licorCoco-malibu")
+const licorMelonMidory = document.getElementById("bottle-img-licorMelon-midory")
+const vermutJerezLustau = document.getElementById("bottle-img-vermut-de-jerez-lustau")
+const whiskeyJackDaniels = document.getElementById("bottle-img-wiskey-jack-daniels")
+const vermutExtraSecoMartini = document.getElementById("bottle-img-vermut-martini-extra-dry")
+const bailis = document.getElementById("bottle-img-baileys")
+const bloddyMaryDrink = document.getElementById("bloddyMaryDrink")
+const daiquiriClassicDrink = document.getElementById("daiquiriClassicDrink")
+const frozenDaiquiriStrowberryDrink = document.getElementById("frozenDaiquiriStrowberryDrink")
+const LongBlueIslandDrink = document.getElementById("LongBlueIslandDrink")
+const maiThaiDrink = document.getElementById("maiThaiDrink")
+const margaritaDrink = document.getElementById("margaritaDrink")
+const martiniDrink = document.getElementById("martiniDrink")
+const mojitoDrink = document.getElementById("mojitoDrink")
+const negroniDrink = document.getElementById("negroniDrink")
+const pinaColadaDrink = document.getElementById("pinaColadaDrink")
+const whiteRussianDrink = document.getElementById("whiteRussianDrink")
+const anisLicor = document.getElementById("bottle-img-licorAnis-dulce-mico")
+const casadorez = document.getElementById("bottle-img-tequila-bco-cazadores")
+
+
+
+
+
+
+
+const bloodyMaryButton = document.getElementById("button-img-bloody-mary")
+const daiquiriClasicoButton = document.getElementById("button-img-daiquiri-clasico")
+const frozenDaiquiriButton = document.getElementById("button-img-frozen-daiquiri")
+const maiThaiButton = document.getElementById("button-img-mai-thai")
+const margaritaButton = document.getElementById("button-img-margarita")
+const martiniButton = document.getElementById("button-img-martini")
+const mojitoButton = document.getElementById("button-img-mojito")
+const negroniButton = document.getElementById("button-img-negroni")
+const whiteRussianButton = document.getElementById("button-img-white-russian")
+
+let displayBloodyMary = document.getElementById("display-bloody-mary")
+let displayDaiquiriClasico = document.getElementById("display-daiquiri-clasico")
+let displayDaiquiriFrozen = document.getElementById("display-daiquiri-frozen")
+let displayMaiThay = document.getElementById("display-mai-thay")
+let displaymargatita = document.getElementById("display-margatita")
+let displayMartini = document.getElementById("display-martini")
+let displayMojito = document.getElementById("display-mojito")
+let displayNegroni = document.getElementById("display-negroni")
+let displayWhiteRussian = document.getElementById("display-white-russian")
+
+
+let videoBloodyMary = document.getElementById("video--bloody-mary")
+let videoDaikiryClasico = document.getElementById("video-daikiri-clasico")
+let videoDaiquiriFrozen = document.getElementById("video-daiquiri-frozen")
+let videoMaiThai = document.getElementById("video-mai-thai")
+let videoMargatita = document.getElementById("video-margatita")
+let videoMartiniSucio = document.getElementById("video-martini-sucio")
+let videoMojito = document.getElementById("video-mojito")
+let videoNegroni = document.getElementById("video-negroni")
+let videoWhiteRussian = document.getElementById("video-white-russian")
+
+bloodyMaryButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="block"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="none"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="block"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="none"
+})
+
+daiquiriClasicoButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="block"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="none"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="block"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="none"
+
+})
+
+frozenDaiquiriButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="block"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="none"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="block"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="none"
+})
+
+maiThaiButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="block"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="none"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="block"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="none"
+})
+
+margaritaButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="block"
+    displayMartini.style.display="none"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="block"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="none"
+
+})
+
+martiniButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="block"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="block"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="none"
+})
+
+mojitoButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="none"
+    displayMojito.style.display="block"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="block"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="none"
+})
+
+negroniButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="none"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="block"
+    displayWhiteRussian.style.display="none"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="block"
+    videoWhiteRussian.style.display="none"
+})
+
+whiteRussianButton.addEventListener("click", ()=>{
+
+    displayBloodyMary.style.display="none"
+    displayDaiquiriClasico.style.display="none"
+    displayDaiquiriFrozen.style.display="none"
+    displayMaiThay.style.display="none"
+    displaymargatita.style.display="none"
+    displayMartini.style.display="none"
+    displayMojito.style.display="none"
+    displayNegroni.style.display="none"
+    displayWhiteRussian.style.display="block"
+
+    videoBloodyMary.style.display="none"
+    videoDaikiryClasico.style.display="none"
+    videoDaiquiriFrozen.style.display="none"
+    videoMaiThai.style.display="none"
+    videoMargatita.style.display="none"
+    videoMartiniSucio.style.display="none"
+    videoMojito.style.display="none"
+    videoNegroni.style.display="none"
+    videoWhiteRussian.style.display="block"
+})
